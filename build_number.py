1d5a9b727dbb018e2c508891f07636be02e18baf
lovedj0166 <lovedj0166@sina.com>
2016-03-30 00:42:22 +0800

    fix

--- build_number.py
+++ build_number.py
@@ -7,9 +7,9 @@ d = i.day
 local_build_number = str(y)[-1] + "." + str(m) + "." + str(d)
 
 if len(os.sys.argv) == 1:
-    build_number = local_build_number
+    build_number = local_build_number #不输入参数使用当前日期作为版本号
 elif len(os.sys.argv) == 2:
-    build_number = os.sys.argv[1]
+    build_number = os.sys.argv[1]     #输入版本号参数
 
 print "Build_Number:" + build_number
 
