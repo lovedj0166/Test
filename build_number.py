f5f1820371d1c1e5ff60ff3dcc087f1367a40706
dingjie <dingjie@xiaomi.com>
2016-03-30 19:18:26 +0800

    fix

--- build_number.py
+++ build_number.py
@@ -1,15 +1,17 @@
 import datetime
 import sys,os
+
 i = datetime.datetime.now()
 y = i.year
 m = i.month
 d = i.day
 local_build_number = str(y)[-1] + "." + str(m) + "." + str(d)
 
-if len(os.sys.argv) == 1:
-    build_number = local_build_number #不输入参数使用当前日期作为版本号
-elif len(os.sys.argv) == 2:
-    build_number = os.sys.argv[1]     #输入版本号参数
-
-print "Build_Number:" + build_number
-
+def buildNumber():
+    if len(os.sys.argv) == 1:
+        build_number = local_build_number
+    elif len(os.sys.argv) == 2:
+        build_number = os.sys.argv[1]
+    return build_number
+if __name__ == '__main__':
+    print buildNumber()
