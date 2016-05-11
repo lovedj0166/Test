ef0caf8bd14bd6ac119267b9cbf6cd9c3988507e
dingjie <dingjie@xiaomi.com>
2016-05-11 13:17:21 +0800

    wget图片

---
+++ kindlecover.py
@@ -0,0 +1,3 @@
+import os
+for i in range(10):
+    os.system("wget -P pic/ http://placeimg.com/768/1024/any/grayscale")
---
+++ split.py
@@ -0,0 +1,3 @@
+name = "123_456_789_0"
+print name.split("_")
+print name.split("_")[-1]
